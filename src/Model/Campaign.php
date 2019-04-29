e792f5cf581b2f3426509cd77f37471d13d9f42e
Florian Thoma <florian.thoma@innoweb.com.au>
2019-04-29 11:38:10 +1000

    update campaign CMS fields to make it clearer what can be changed

--- src/Model/Campaign.php
+++ src/Model/Campaign.php
@@ -3,6 +3,10 @@
 namespace Innoweb\MailChimpSignup\Model;
 
 use Innoweb\MailChimpSignup\Pages\CampaignListPage;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\DateField;
+use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataObject;
 
 class Campaign extends DataObject {
@@ -10,14 +14,14 @@ class Campaign extends DataObject {
     private static $table_name = 'MailChimpSignupCampaign';
 
     private static $db = [
-        'MailChimpID'   =>  'Varchar(255)',
-        'Title'         =>  'Varchar(255)',
-        'Subject'       =>  'Varchar(255)',
-        'SentDate'      =>  'Datetime',
-        'URL'           =>  'Varchar(1024)',
-        'Hidden'        =>  'Boolean',
-        'ListID'        =>  'Varchar(50)',
-        'SentToSegment' =>  'Boolean'
+        'MailChimpID'       =>  'Varchar(255)',
+        'Title'             =>  'Varchar(255)',
+        'Subject'           =>  'Varchar(255)',
+        'SentDate'          =>  'Datetime',
+        'URL'               =>  'Varchar(1024)',
+        'Hidden'            =>  'Boolean',
+        'ListID'            =>  'Varchar(50)',
+        'SentToSegment'     =>  'Boolean'
     ];
 
     private static $has_one = [
@@ -25,10 +29,10 @@ class Campaign extends DataObject {
     ];
 
     private static $summary_fields = [
-        'Title'         =>  'Title',
-        'Subject'       =>  'Subject',
-        'SentDate.Nice' =>  'Sent',
-        'Status'        =>  'Status'
+        'Title'             =>  'Title',
+        'Subject'           =>  'Subject',
+        'SentDate.Nice'     =>  'Sent',
+        'Status'            =>  'Status'
     ];
 
     private static $casting = [
@@ -36,12 +40,46 @@ class Campaign extends DataObject {
     ];
 
     private static $default_sort = 'SentDate DESC';
+    
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function ($fields) {
+            
+            $fields->removeByName('PageID');
+            $fields->removeByName('Title');
+            $fields->removeByName('Subject');
+            $fields->removeByName('Hidden');
+            $fields->removeByName('SentDate');
+            $fields->removeByName('URL');
+            $fields->removeByName('MailChimpID');
+            $fields->removeByName('ListID');
+            $fields->removeByName('SentToSegment');
+            
+            $fields->addFieldsToTab(
+                'Root.Main',
+                [
+                    ReadonlyField::create('Title', 'Campaign Title'),
+                    TextField::create('Subject', 'Subject'),
+                    CheckboxField::create('Hidden', 'Hide from page'),
+                    DateField::create('SentDate', 'Date sent')->performDisabledTransformation(),
+                    ReadonlyField::create('URL', 'URL'),
+                    ReadonlyField::create('MailChimpID', 'Mailchimp ID'),
+                    ReadonlyField::create('ListID', 'Mailchimp List ID'),
+                    CheckboxField::create('SentToSegment', 'Sent to segment')->performDisabledTransformation(),
+                ]
+            );
+        });
+            
+        $fields = parent::getCMSFields();
+        
+        return $fields;
+    }
 
     public function getLink()
     {
         return $this->URL;
     }
-
+    
     public function getStatus()
     {
         $page = $this->Page();
